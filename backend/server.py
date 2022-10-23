f1286856624746c048763a70da93eb541859b1da
elikkozak <elik.kozak@gmail.com>
2022-10-23 09:52:24 +0300

    created template for endpoint

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+import requests
 
 
 app = FastAPI()
@@ -9,9 +10,26 @@ app = FastAPI()
 app.mount("/client/build", StaticFiles(directory="client/build"), name="static")
 
 
+@app.get("/sanity")
+def root():
+    return "OK"
+
+
 @app.get("/")
 def root():
     return FileResponse("./client/build/index.html")
+
+
+@app.get("/")
+def get_recipes(ingredient, is_gluten, is_dairy):
+    recipes = requests.get(
+        f"https://recipes-goodness.herokuapp.com/recipes/cheese").json()
+    if is_gluten:
+        recipes= get_non_gluten_recipes(recipes)
+    if is_dairy:
+        recipes= get_non_dairy_recipes(recipes)
+
+    return recipes
 
 
 if __name__ == "__main__":
---
+++ backend/server_utils.py
@@ -0,0 +1,9 @@
+
+
+
+def get_non_gluten_recipes(recipes):
+    pass
+
+
+def get_non_dairy_recipes(recipes):
+    pass
