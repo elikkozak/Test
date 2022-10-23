482f4420694450c7e1aace3083314a3ed456d916
elikkozak <elik.kozak@gmail.com>
2022-10-23 12:51:02 +0300

    cleaned code ready for submit, maybe will add more styling

--- client/build/view.js
+++ client/build/view.js
@@ -8,15 +8,3 @@ class ViewModule {
         $(".recipes-container").append(newHTML);
     }
 }
-//     menu: [
-//       { name: "Google", link: "http://google.com" },
-//       { name: "Facebook", link: "http://facebook.com" },
-//       { name: "Instagram", link: "http://nstagram.com" },
-//       { name: "Twitter", link: "http://twitter.com" },
-//     ]
-//   };
-//   const source = $('#menu-template').html();
-//   const template = Handlebars.compile(source);
-//   const newHTML = template(menuData);
-//   // append our new html to the page
-//   $('.menu').append(newHTML);
--- client/src/view.ts
+++ client/src/view.ts
@@ -6,24 +6,8 @@ class ViewModule{
         const newHTML = template(result);
         $(".recipes-container").append(newHTML);
 
-        
+    
     }
 
 
 }
-
-
-//     menu: [
-//       { name: "Google", link: "http://google.com" },
-//       { name: "Facebook", link: "http://facebook.com" },
-//       { name: "Instagram", link: "http://nstagram.com" },
-//       { name: "Twitter", link: "http://twitter.com" },
-//     ]
-//   };
-  
-//   const source = $('#menu-template').html();
-//   const template = Handlebars.compile(source);
-//   const newHTML = template(menuData);
-  
-//   // append our new html to the page
-//   $('.menu').append(newHTML);
