d5d80fbd660116e09e3bcbcbae6b7086813096dd
elikkozak <elik.kozak@gmail.com>
2022-10-23 12:48:29 +0300

    created alert for first ingredient

--- client/build/controller.js
+++ client/build/controller.js
@@ -10,4 +10,12 @@
             render.renderPage(result);
         });
     });
+    $(".recipes-container").on("click", ".recipe-img", function (e) {
+        if (e.target && e.target.parentElement.className === "recipe-card") {
+            const ingredients = e.target.parentElement.children[2];
+            const ingredientsList = ingredients.getElementsByTagName('ul')[0];
+            const ingredientName = ingredientsList.getElementsByTagName('li')[0].textContent;
+            alert(ingredientName);
+        }
+    });
 })();
